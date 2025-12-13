1a350fec518bb5ebf7d0bbe74f6b9c5c34e9c2e1
lihuagang <bert825_work@163.com>
2025-12-14 02:09:48 +0800

    Streaming-流式聊天

--- tutorials/src/main/java/_04_Streaming.java
+++ tutorials/src/main/java/_04_Streaming.java
@@ -1,3 +1,4 @@
+import dev.langchain4j.model.chat.StreamingChatModel;
 import dev.langchain4j.model.chat.response.ChatResponse;
 import dev.langchain4j.model.chat.response.StreamingChatResponseHandler;
 import dev.langchain4j.model.openai.OpenAiStreamingChatModel;
@@ -11,18 +12,24 @@ public class _04_Streaming {
 
     public static void main(String[] args) {
 
-        OpenAiStreamingChatModel model = OpenAiStreamingChatModel.builder()
+        // 流式聊天模型
+        StreamingChatModel model = OpenAiStreamingChatModel.builder()
                 .apiKey(ApiKeys.OPENAI_API_KEY)
                 .modelName(GPT_4_O_MINI)
                 .build();
 
+        // 提示词
         String prompt = "Write a short funny poem about developers and null-pointers, 10 lines maximum";
 
         System.out.println("Nr of chars: " + prompt.length());
+        // 词元计数估算器
         System.out.println("Nr of tokens: " + new OpenAiTokenCountEstimator(GPT_4_O_MINI).estimateTokenCountInText(prompt));
 
+        // 异步的聊天响应
         CompletableFuture<ChatResponse> futureChatResponse = new CompletableFuture<>();
 
+        // 流式聊天
+        // 流式聊天响应处理器
         model.chat(prompt, new StreamingChatResponseHandler() {
 
             @Override
@@ -33,15 +40,18 @@ public class _04_Streaming {
             @Override
             public void onCompleteResponse(ChatResponse completeResponse) {
                 System.out.println("\n\nDone streaming");
+                // 聊天响应
                 futureChatResponse.complete(completeResponse);
             }
 
             @Override
             public void onError(Throwable error) {
+                // 错误发生
                 futureChatResponse.completeExceptionally(error);
             }
         });
 
+        // 完成时返回结果值，如果异常完成则抛出（未检查的）异常。
         futureChatResponse.join();
     }
 }
