2f1de7d700ab0bf3cd4966be16257fd5571d35e6
lihuagang03 <bert825_work@163.com>
2025-12-15 16:46:09 +0800

    Advanced_RAG_with_Metadata_Example-具有元数据的高级RAG示例

--- rag-examples/src/main/java/_3_advanced/_04_Advanced_RAG_with_Metadata_Example.java
+++ rag-examples/src/main/java/_3_advanced/_04_Advanced_RAG_with_Metadata_Example.java
@@ -28,13 +28,18 @@ import static dev.langchain4j.model.openai.OpenAiChatModelName.GPT_4_O_MINI;
 import static java.util.Arrays.asList;
 import static shared.Utils.*;
 
+/**
+ * 具有元数据的高级RAG示例
+ */
 public class _04_Advanced_RAG_with_Metadata_Example {
 
     /**
+     * 请参考 Naive_RAG_Example 以获取基础上下文。
      * Please refer to {@link Naive_RAG_Example} for a basic context.
      * <p>
      * Advanced RAG in LangChain4j is described here: https://github.com/langchain4j/langchain4j/pull/538
      * <p>
+     * 此示例说明了如何将文档来源和其他元数据包含到大型语言模型提示中。
      * This example illustrates how to include document source and other metadata into the LLM prompt.
      */
 
@@ -63,20 +68,24 @@ public class _04_Advanced_RAG_with_Metadata_Example {
 
         ingestor.ingest(document);
 
+        // 嵌入存储的内容检索器
         ContentRetriever contentRetriever = EmbeddingStoreContentRetriever.builder()
                 .embeddingStore(embeddingStore)
                 .embeddingModel(embeddingModel)
                 .build();
 
+        // 每个检索到的片段在提示中都应该包含“file_name”和“index”元数据值
         // Each retrieved segment should include "file_name" and "index" metadata values in the prompt
+        // 内容注入器
         ContentInjector contentInjector = DefaultContentInjector.builder()
                 // .promptTemplate(...) // Formatting can also be changed
                 .metadataKeysToInclude(asList("file_name", "index"))
                 .build();
 
+        // 检索增强器
         RetrievalAugmentor retrievalAugmentor = DefaultRetrievalAugmentor.builder()
                 .contentRetriever(contentRetriever)
-                .contentInjector(contentInjector)
+                .contentInjector(contentInjector) // 内容注入器
                 .build();
 
         ChatModel chatModel = OpenAiChatModel.builder()
@@ -87,7 +96,7 @@ public class _04_Advanced_RAG_with_Metadata_Example {
 
         return AiServices.builder(Assistant.class)
                 .chatModel(chatModel)
-                .retrievalAugmentor(retrievalAugmentor)
+                .retrievalAugmentor(retrievalAugmentor) // 检索增强器
                 .chatMemory(MessageWindowChatMemory.withMaxMessages(10))
                 .build();
     }
